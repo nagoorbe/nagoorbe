19cf2c2568bd906dcab4e3ac6f8f5fe442547be0
nagoorbe <nagoorbe51@gmail.com>
2025-09-17 08:29:11 +0530

    Add files via upload

---
+++ reversestr.py
@@ -0,0 +1,11 @@
+str=input("enter the string")
+print(str[::1])
+
+#another method
+str=input("enter the string")
+print(str[::-1])
+
+
+# another method
+str=input("enter the string")
+print(str[1::])
