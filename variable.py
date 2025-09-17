aab9db3a24969928ccb8cc97972991aad074d040
nagoorbe <nagoorbe51@gmail.com>
2025-09-17 08:27:45 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+a=10
+b=5
+print("before swapping a=",a  ,"b=",b)
+
+a,b=b,a
+print("after swapping a=",a  ,"b=",b) 
+
+
+
+a,b,=10,5
+print("before swapping a=",a  ,"b=",b)
+c=a
+a=b
+b=c
+print("after swapping a=",a  ,"b=",b) 
+
+
+x=15
+y=30
+print("sum of x and y is",x+y)
