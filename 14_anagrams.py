5dbc19cab02ff7b9cde38349d9280149361cac90
nagoorbe <nagoorbe51@gmail.com>
2025-10-01 21:11:04 +0530

    Add files via upload

---
+++ 14_anagrams.py
@@ -0,0 +1,5 @@
+def is_anagram(s1, s2):
+    return sorted(s1.replace(" ", "").lower()) == sorted(s2.replace(" ", "").lower())
+
+print(is_anagram("listen", "silent"))   # True
+print(is_anagram("hello", "world"))     # False
